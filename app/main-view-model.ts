854c4668649c45d866ff5a54a2bd43143c20a425
Julio Tain Sueiras <juliosueiras@gmail.com>
2017-04-21 17:56:20 -0400

    Added Progressbar, and Change style for actionbar

--- app/main-view-model.ts
+++ app/main-view-model.ts
@@ -3,11 +3,12 @@ import {Zip} from "nativescript-zip";
 import * as http from 'http'
 import * as fs from 'file-system'
 import * as frameModule from 'ui/frame'
+import {LoadingIndicator} from "nativescript-loading-indicator"
 let folder = fs.knownFolders.documents()
 
 export class ViewModel extends Observable {
     private _officialDocList = [
-        {"docName": "Bash","docPath": "https://github.com/juliosueiras/temp_dashdocs_feed/blob/master/Bash.zip?raw=true", "docWebName": "bash/index.html"},
+        {"docName": "Bash","docPath": "https://github.com/juliosueiras/temp_dashdocs_feed/blob/master/Bash.zip?raw=true", "docWebIndex": "bash/index.html"},
         {"docName": "Ansible","docPath": "https://github.com/juliosueiras/temp_dashdocs_feed/blob/master/Ansible.zip?raw=true", "docWebIndex": "docs.ansible.com/ansible/index.html"},
         {"docName": "BackboneJS","docPath": "https://github.com/juliosueiras/temp_dashdocs_feed/blob/master/BackboneJS.zip?raw=true", "docWebIndex": "index.html"},
         {"docName": "AngularJS","docPath": "https://github.com/juliosueiras/temp_dashdocs_feed/blob/master/AngularJS.zip?raw=true", "docWebIndex": "angular.io/docs/js/latest/index.html"},
@@ -56,13 +57,31 @@ export class ViewModel extends Observable {
     }
 
     public onItemTap(args) {
+        let loader = new LoadingIndicator();
         let docPath = this._officialDocList[args.index].docPath
         let docName = this._officialDocList[args.index].docName
         let zipPath = fs.path.join(fs.knownFolders.temp().path,  docName + ".zip")
         let dest = fs.path.join(fs.knownFolders.temp().path,  "/" + docName)
 
+        loader.show(); 
+
+
+		if(fs.File.exists(dest)) {
+            this.changeToDocView(dest, docName, args)
+            loader.hide();
+            return
+        }
+
         http.getFile(docPath, zipPath).then((res) => {
             Zip.unzip(zipPath, dest);
+            this.changeToDocView(dest, docName, args)
+            loader.hide();
+        }).catch(function(e) {
+            console.log(e)
+        })
+    }
+
+	private changeToDocView(dest: string, docName: string, args) {
             this._currentDocPath = dest + '/' + docName + '.docset/Contents/Resources/Documents/' + this._officialDocList[args.index].docWebIndex
             console.log(this._currentDocPath)
             frameModule.topmost().navigate({
@@ -73,10 +92,7 @@ export class ViewModel extends Observable {
                     docPath: this._currentDocPath
                 }
             })
-        }).catch(function(e) {
-            console.log(e)
-        })
-    }
+	}
 
     public getPathToIndex(args) {
         console.log(args)
