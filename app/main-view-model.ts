012e9b229bbb151465f1ec973c04b541df13d314
Julio Tain Sueiras <juliosueiras@gmail.com>
2017-04-21 12:40:55 -0400

    Update

--- app/main-view-model.ts
+++ app/main-view-model.ts
@@ -7,16 +7,17 @@ let folder = fs.knownFolders.documents()
 
 export class ViewModel extends Observable {
     private _officialDocList = [
-        {"docName": "Bash","docPath": "https://github.com/juliosueiras/temp_dashdocs_feed/blob/master/Bash.zip?raw=true", "docWebName": "bash/index.html"},
+        {"docName": "Bash","docPath": "https://github.com/juliosueiras/temp_dashdocs_feed/blob/master/Bash.zip?raw=true", "docWebIndex": "bash/index.html"},
         {"docName": "Ansible","docPath": "https://github.com/juliosueiras/temp_dashdocs_feed/blob/master/Ansible.zip?raw=true", "docWebIndex": "docs.ansible.com/ansible/index.html"},
         {"docName": "BackboneJS","docPath": "https://github.com/juliosueiras/temp_dashdocs_feed/blob/master/BackboneJS.zip?raw=true", "docWebIndex": "index.html"},
         {"docName": "AngularJS","docPath": "https://github.com/juliosueiras/temp_dashdocs_feed/blob/master/AngularJS.zip?raw=true", "docWebIndex": "angular.io/docs/js/latest/index.html"},
+        {"docName": "Cordova","docPath": "https://github.com/juliosueiras/temp_dashdocs_feed/blob/master/Cordova.zip?raw=true", "docWebIndex": "cordova.apache.org/docs/en/latest/index.html"},
     ]
 
     private _currentDocPath;
 
     private _userDocList = [
-        {"docName": "SDL","docPath": "https://github.com/juliosueiras/temp_dashdocs_feed/blob/master/SDL.zip?raw=true", "docWebIndex": "index.html"}
+        {"docName": "SDL","docPath": "org.libsdl.sdl20.docset", "docWebIndex": "index.html"}
     ]
 
 
@@ -32,34 +33,11 @@ export class ViewModel extends Observable {
         return this._userDocList
     }
 
-    public onItemTapUser(args) {
-        let docPath = this._userDocList[args.index].docPath
-        let docName = this._userDocList[args.index].docName
-        let zipPath = fs.path.join(fs.knownFolders.temp().path,  docName + ".zip")
-        let dest = fs.path.join(fs.knownFolders.temp().path,  "/" + docName)
-
-        http.getFile(docPath, zipPath).then((res) => {
-            Zip.unzip(zipPath, dest);
-            this._currentDocPath = dest + '/' + docName + '.docset/Contents/Resources/Documents/' + this._userDocList[args.index].docWebIndex
-            console.log(this._currentDocPath)
-            frameModule.topmost().navigate({
-                moduleName: 'doc-view',
-                backstackVisible: true,
-                context: {
-                    docName: docName,
-                    docPath: this._currentDocPath
-                }
-            })
-        }).catch(function(e) {
-            console.log(e)
-        })
-    }
-
     public onItemTap(args) {
         let docPath = this._officialDocList[args.index].docPath
         let docName = this._officialDocList[args.index].docName
-        let zipPath = fs.path.join(fs.knownFolders.temp().path,  docName + ".zip")
-        let dest = fs.path.join(fs.knownFolders.temp().path,  "/" + docName)
+        let zipPath = "/sdcard/" + docName + ".zip"
+        let dest = "/sdcard/" + docName
 
         http.getFile(docPath, zipPath).then((res) => {
             Zip.unzip(zipPath, dest);
@@ -74,7 +52,7 @@ export class ViewModel extends Observable {
                 }
             })
         }).catch(function(e) {
-            console.log(e)
+            console.dump(e)
         })
     }
 
